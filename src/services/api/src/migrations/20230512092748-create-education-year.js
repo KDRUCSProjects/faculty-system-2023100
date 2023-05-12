da39bfafa6154d8f91d96a97e5ce5aa3131a9290
shamsullah-shams <shamsullah07073@gmail.com>
2023-05-12 13:59:38 +0430

    Educational Year model and migration are created

---
+++ src/services/api/src/migrations/20230512092748-create-education-year.js
@@ -0,0 +1,28 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('EducationYears', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      year: {
+        type: Sequelize.NUMBER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('EducationYears');
+  }
+};
