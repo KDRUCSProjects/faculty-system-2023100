a17663b4c5977bc709e9f8c08fcbffd51d91c305
zubair-khairy <72171708+zubair-khairy@users.noreply.github.com>
2023-05-13 23:40:44 +0430

    tajil is created

---
+++ src/services/api/src/models/taajil.js
@@ -0,0 +1,31 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Taajil extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+
+    static associate(models) {
+      // define association here
+    }
+  }
+  Taajil.init(
+    {
+      SerialID: DataTypes.STRING,
+      Year: DataTypes.DATE,
+      Studnet: DataTypes.STRING,
+      Term: DataTypes.STRING,
+      StartDate: DataTypes.STRING,
+      HasEnded: DataTypes.BOOLIAN,
+    },
+    {
+      sequelize,
+      modelName: 'Taajil',
+      timestamps: true,
+    }
+  );
+  return Taajil;
+};
