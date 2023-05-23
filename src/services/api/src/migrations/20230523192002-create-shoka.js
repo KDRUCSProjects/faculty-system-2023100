d127ca88148ab73a0de2613a9d0a0b7f736dd0da
Mohammad Nabi <mnabimd@gmail.com>
2023-05-24 00:12:18 +0430

    datatype bug fixed

--- src/services/api/src/migrations/20230523192002-create-shoka.js
+++ src/services/api/src/migrations/20230523192002-create-shoka.js
@@ -10,7 +10,7 @@ module.exports = {
         autoIncrement: true,
       },
       subjectId: {
-        type: DataTypes.INTEGER,
+        type: Sequelize.INTEGER,
         required: true,
         trim: true,
         references: {
