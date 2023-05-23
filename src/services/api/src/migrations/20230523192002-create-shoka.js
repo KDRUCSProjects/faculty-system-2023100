9d28aa3a86251f2be8332f70c4c2d55f8af099fd
Mohammad Nabi <mnabimd@gmail.com>
2023-05-23 23:54:56 +0430

    Shoka Model

---
+++ src/services/api/src/migrations/20230523192002-create-shoka.js
@@ -0,0 +1,29 @@
+const BaseModel = require('../models/basemodel');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Shokas', {
+      id: {
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+      },
+      subjectId: {
+        type: DataTypes.INTEGER,
+        required: true,
+        trim: true,
+        references: {
+          model: 'Subjects',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+      ...BaseModel(Sequelize),
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Shokas');
+  },
+};
---
+++ src/services/api/src/models/shoka.js
@@ -0,0 +1,42 @@
+const { Model } = require('sequelize');
+const BaseModel = require('./basemodel');
+
+module.exports = (sequelize, DataTypes) => {
+  class Shoka extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Subject, { foreignKey: 'subjectId' });
+    }
+  }
+  Shoka.init(
+    {
+      id: {
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+      },
+      subjectId: {
+        type: DataTypes.INTEGER,
+        required: true,
+        trim: true,
+        references: {
+          model: 'Subject',
+          key: 'id',
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+      ...BaseModel(DataTypes),
+    },
+    {
+      sequelize,
+      modelName: 'Shoka',
+    }
+  );
+  return Shoka;
+};
