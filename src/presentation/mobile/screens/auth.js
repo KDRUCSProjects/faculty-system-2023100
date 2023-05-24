9793617eb2af2b6a3aafa3d0cf4953e81180313d
malik <samimi.malik7@gmail.com>
2023-05-24 09:43:43 +0430

    Loading Screen UI design Implemented

--- src/presentation/mobile/screens/auth.js
+++ src/presentation/mobile/screens/auth.js
@@ -1,7 +1,8 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, Image } from "react-native";
 import { ActivityIndicator } from "react-native-paper";
+
 import { useSelector } from "react-redux";
 
 export default function auth(props) {
@@ -9,14 +10,19 @@ export default function auth(props) {
     const tryLogin = async () => {
       const userData = await AsyncStorage.getItem("userData");
       if (!userData) {
-        props.navigation.navigate("Login");
+        setTimeout(() => {
+          props.navigation.navigate("Login");
+        }, 3000);
         return;
       }
       const transformedData = JSON.parse(userData);
       const { token, userId, expirationDate } = transformedData;
       const tExpirationDate = new Date(expirationDate);
       if (tExpirationDate <= new Date() || !token || !userId) {
-        props.navigation.navigate("Login");
+        setTimeout(() => {
+          props.navigation.navigate("Login");
+        }, 3000);
+
         return;
       }
       props.navigation.navigate("teacherScreen");
@@ -25,14 +31,21 @@ export default function auth(props) {
   }, []);
   return (
     <View style={styles.container}>
-      <ActivityIndicator size={"large"}></ActivityIndicator>
+      <Image
+        source={require("../assets/images/university_student.png")}
+        style={{ height: 500, width: 141, overflow: "hidden", margin: 10 }}
+      ></Image>
+
+      <ActivityIndicator size={"large"} color="#5DA3FF"></ActivityIndicator>
     </View>
   );
 }
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
+    justifyContent: "space-around",
     alignItems: "center",
+    height: "100%",
+    width: "100%",
   },
 });
