c5dc46cfb186933a942bc09ee6d4a7d6d33ddab7
shamsullah-shams <shamsullah07073@gmail.com>
2023-05-27 09:35:02 +0430

    swagger docs for subject CRUD end points

--- src/services/api/src/routes/subject.routes.js
+++ src/services/api/src/routes/subject.routes.js
@@ -8,12 +8,142 @@ const router = express.Router();
 
 router
   .route('/')
-  .get(subjectController.getSubjects)
-  .post(validate(subjectValidations.createSubject), subjectController.createSubject);
+  .get(auth(), subjectController.getSubjects)
+  .post(auth(), validate(subjectValidations.createSubject), subjectController.createSubject);
 
 router
   .route('/:subjectId')
-  .get(validate(subjectValidations.getSubject), subjectController.getSubject)
-  .delete(validate(subjectValidations.getSubject), subjectController.deleteSubject);
+  .get(auth(), validate(subjectValidations.getSubject), subjectController.getSubject)
+  .delete(auth(), validate(subjectValidations.getSubject), subjectController.deleteSubject);
 
 module.exports = router;
+
+/**
+ * @swagger
+ * tags:
+ *   name: Subject
+ *   description: Subject management and retrieval
+ */
+
+/**
+ * @swagger
+ * /subjects:
+ *   get:
+ *     summary: get all subjects
+ *     description: Get All Subjects.
+ *     tags: [Subject]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 results:
+ *                   type: array
+ *                   items:
+ *                     $ref: '#/components/schemas/Subject'
+ *
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /subjects:
+ *   post:
+ *     summary: Create a Subject alongside Shoka and Attendance
+ *     description: Add a new subject.
+ *     tags: [Subject]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - name
+ *             properties:
+ *               name:
+ *                 type: string
+ *               semesterId:
+ *                 type: number
+ *               teacherId:
+ *                 type: number
+ *             example:
+ *               name: Big Data
+ *               semesterId: 8
+ *               teacherId: 3
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Subject'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ */
+
+/**
+ * @swagger
+ * /subjects/{id}:
+ *   get:
+ *     summary: Get an Subject
+ *     description: get single subject based on ID.
+ *     tags: [Subject]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: subject id
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/Subject'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *
+ *   delete:
+ *     summary: Delete a Subject
+ *     description: Delete a subject based on Id.
+ *     tags: [Subject]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: number
+ *         description: subject id
+ *     responses:
+ *       "200":
+ *         description: No content
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ */
