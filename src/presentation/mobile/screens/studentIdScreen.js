b55f8affaa2ca04e87090a1927782aee7a90dc00
malik <samimi.malik7@gmail.com>
2023-05-30 11:06:06 +0430

    Teacher and Student Login Switcher implemented with UI design

--- src/presentation/mobile/screens/studentIdScreen.js
+++ src/presentation/mobile/screens/studentIdScreen.js
@@ -1,73 +1,111 @@
 import React, { useState } from "react";
+import { Alert, ImageBackground, SafeAreaView } from "react-native";
 import {
   StyleSheet,
-  Text,
   View,
   Image,
   TextInput,
   Button,
+  Pressable,
+  Text,
   TouchableOpacity,
-  ImageBackground,
+  BackHandler,
 } from "react-native";
+import {
+  ScrollView,
+  TouchableWithoutFeedback,
+} from "react-native-gesture-handler";
+import { useDispatch } from "react-redux";
+import { authenticate } from "../store/actions/actions";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
+import { useTogglePasswordVisibility } from "../hooks/useTogglePasswordVisibility";
 import colors from "../constants/colors";
+import { useEffect } from "react";
+import { ActivityIndicator, shadow } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
+import {
+  Layout,
+  TopNavigation,
+  Divider,
+  BottomNavigation,
+  BottomNavigationTab,
+} from "@ui-kitten/components";
+import { LinearGradient } from "expo-linear-gradient";
+
 export default studentIdScreen = (props) => {
-  const [id, setid] = useState("");
+  const onTeacher = () => props.navigation.navigate("Login");
 
   return (
-    <View style={styles.container}>
-      <View style={styles.markImage}>
-        <ImageBackground
-          source={require("../assets/images/marks.png")}
-          style={{ width: 100, height: 100 }}
-        ></ImageBackground>
-      </View>
-      <View style={styles.inputView}>
-        <TextInput
-          style={styles.TextInput}
-          placeholder="Student ID"
-          placeholderTextColor="white"
-          onChangeText={(id) => setid(id)}
-        />
-      </View>
-
-      <TouchableOpacity style={styles.submitbutton}>
-        <Text style={styles.loginText}>Submit</Text>
-      </TouchableOpacity>
-    </View>
+    <ScrollView style={{ flex: 1 }} contentContainerStyle={{ flexGrow: 1 }}>
+      <LinearGradient
+        colors={["#5DA3FF", "#00157D"]}
+        style={styles.container}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+      >
+        <View
+          style={{
+            height: 50,
+            width: "70%",
+            borderRadius: 15,
+            backgroundColor: "white",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            marginBottom: "5%",
+          }}
+        >
+          <TouchableOpacity
+            onPress={onTeacher}
+            style={{
+              width: "50%",
+              justifyContent: "center",
+              borderRadius: 15,
+              backgroundColor: "white",
+            }}
+          >
+            <Text
+              style={{
+                ...styles.Text,
+                textAlign: "center",
+                color: "#EB6A70",
+              }}
+            >
+              Teacher
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{
+              width: "50%",
+              justifyContent: "center",
+              borderRadius: 15,
+              backgroundColor: "#EB6A70",
+            }}
+          >
+            <Text
+              style={{
+                ...styles.Text,
+                textAlign: "center",
+              }}
+            >
+              Student
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </LinearGradient>
+    </ScrollView>
   );
 };
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
+    justifyContent: "flex-end",
+    width: "100%",
+    height: "100%",
     flex: 1,
-    backgroundColor: colors.primary,
-    alignItems: "center",
-    justifyContent: "space-evenly",
   },
-  markImage: { margin: "10%" },
-  inputView: {
-    backgroundColor: colors.secondry,
-    borderRadius: 10,
-    width: "60%",
-    height: 45,
-    marginBottom: 20,
-    alignItems: "center",
-  },
-  TextInput: {
-    height: 50,
-    flex: 1,
-    padding: 10,
-    marginLeft: 20,
-  },
-
-  submitbutton: {
-    width: "80%",
-    borderRadius: 25,
-
-    height: 50,
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 40,
-    backgroundColor: colors.secondry,
+  Text: {
+    fontSize: 18,
+    color: "white",
   },
 });
