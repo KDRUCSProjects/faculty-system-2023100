08f11531c9a7aed6df896981373bfd87687677a3
zubair khairy <zubairy.khairy@gmail.com>
2023-07-22 09:10:03 -0700

    firstUser Wizard setup

---
+++ src/services/api/src/routes/firstUser.routes.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const { createUser } = require('../controllers/firstUser.controller');
+const validate = require('../middlewares/validate');
+const { firstUserValidation,  } = require('../validations');
+const router = express.Router();
+
+// Route for creating users
+router.post('/', createUser);
+
+module.exports = router;
+
+
+
+
+
+/**
+ * @swagger
+ * tags:
+ *   name: first_User
+ *   description: first User setup 
+ */
+
+/**
+ * @swagger
+ * /first_users:
+ *   post:
+ *     summary: Create first user
+ *     description: Only admins can create other users.
+ *     tags: [first_User]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - accounts
+ *               - currentEducationalYear
+ *     properties:
+ *      accounts:
+ *           type: array
+ *                items:
+ *                    type: object
+ *                    required:
+ *                      - name
+ *                      - email
+ *                      - password
+ *                      - role
+ *                    properties:
+ *                      name:
+ *                        type: string
+ *                      email:
+ *                        type: string
+ *                      password:
+ *                       type: string
+ *                      role:
+ *                       type: string
+ *           example:
+ *               accounts:
+ *                 - name: fake name
+ *                   email: fake@example.com
+ *                   password: password1
+ *                   role: user
+ *                 - name: fake name
+ *                   email: fake@example.com
+ *                   password: password1
+ *                   role: user
+ *                 - name: fake name
+ *                   email: fake@example.com
+ *                   password: password1
+ *                   role: user
+ *               currentEducationalYear: 2019
+ *     responses:
+ *       "201":
+ *         description: Created
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ *       "400":
+ *         $ref: '#/components/responses/DuplicateEmail'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *
+ */
+
