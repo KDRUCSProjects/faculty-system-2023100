96ed12ed9d759c8f749675ba76ebb2c3f80c2577
Mohammad Nabi <mnabimd@gmail.com>
2023-09-25 15:26:52 +0430

    extra temporary fields @ students model

--- src/services/api/src/models/student.js
+++ src/services/api/src/models/student.js
@@ -72,6 +72,10 @@ module.exports = (sequelize, DataTypes) => {
         defaultValue: 'male',
       },
       admissionYear: DataTypes.INTEGER,
+      schoolName: DataTypes.STRING,
+      schoolGraduationYear: DataTypes.STRING,
+      monographTitle: DataTypes.STRING,
+      monographDefenseDate: DataTypes.DATE,
       ...BaseModel(DataTypes),
     },
     {
