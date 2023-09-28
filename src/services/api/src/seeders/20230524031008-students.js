a15addafcdec40551cc89b08dce4008debd1ecd1
Mohammad Nabi <mnabimd@gmail.com>
2023-09-28 16:45:07 +0430

    shoka lock added

--- src/services/api/src/seeders/20230524031008-students.js
+++ src/services/api/src/seeders/20230524031008-students.js
@@ -3,41 +3,41 @@ const faker = require('faker');
 // Register students who have succeed in 1401 kankorYear (educationalYearId)
 // And the last two students at 1400 kankor year
 
-// const names = [
-//   'Muhammad',
-//   'Najeeb',
-//   'Ahmad',
-//   'Nisar',
-//   'Bilal',
-//   'Shamsullah',
-//   'M Nabi',
-//   'Ziya',
-//   'Khan',
-//   'Agha Shreen',
-//   'Mahmood',
-//   'Zadran',
-//   'Qudratullah',
-//   'samiullah',
-//   'Baseer Ah',
-// ];
-
 const names = [
-  'محمد',
-  'نجیب الله',
-  'احمد',
-  'نثار',
-  'بلال',
-  'شمس الله',
-  'محمد نبی',
-  'زیا',
-  'خان',
-  'اغا شیرین',
-  'محمود',
-  'زدران',
-  'قدرت الله',
-  'سمیع الله',
-  'بصیر احمد',
+  'Muhammad',
+  'Najeeb',
+  'Ahmad',
+  'Nisar',
+  'Bilal',
+  'Shamsullah',
+  'M Nabi',
+  'Ziya',
+  'Khan',
+  'Agha Shreen',
+  'Mahmood',
+  'Zadran',
+  'Qudratullah',
+  'samiullah',
+  'Baseer Ah',
 ];
+
+// const names = [
+//   'محمد',
+//   'نجیب الله',
+//   'احمد',
+//   'نثار',
+//   'بلال',
+//   'شمس الله',
+//   'محمد نبی',
+//   'زیا',
+//   'خان',
+//   'اغا شیرین',
+//   'محمود',
+//   'زدران',
+//   'قدرت الله',
+//   'سمیع الله',
+//   'بصیر احمد',
+// ];
 
 const students = [...Array(15)].map((student, i) => {
   return {
